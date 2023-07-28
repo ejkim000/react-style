ee0aeea926832e7d3e1e7d08bc6196ca1b5d6cfa
ejkim000 <developer.ejk@gmail.com>
2023-07-28 15:08:22 -0400

    finished tutorial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled';
+import { Container } from './styles/Container.styled';
+import { Flex } from './styles/Flex.styled'
+import { Button } from './styles/Button.styled'
 
 function Header() {
   return (
-    <header>
-        <h1>Header</h1>
-    </header>
+    <StyledHeader>
+        <Container>
+            <Nav>
+                <Logo src='./images/logo.svg' alt='' />
+                <Button>Try It Free</Button>
+            </Nav>
+
+            <Flex>
+                <div>
+                    <h1>Build The Community Your Fans Will Love</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias corrupti aliquam, laboriosam odit quae earum! Dolorum non itaque ipsa aut similique consectetur ipsam facilis hic necessitatibus.</p>
+                    <Button bg='#ff0099' color='#fff'>Get Started For Free</Button>
+                </div>
+
+                <Image src='./images/illustration-mockups.svg' alt='' />
+            </Flex>
+        </Container>
+    </StyledHeader>
   )
 }
 
