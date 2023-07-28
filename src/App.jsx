24423868ef12d0e8ae9059cc8a31e8f98d450adf
ejkim000 <developer.ejk@gmail.com>
2023-07-28 11:16:14 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Container } from './components/styles/Container.style'
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+    <Header>
+      
+    </Header>
+    <Container>
+      <h1>Hello</h1>
+
+    </Container>
+    </>
+  )
+}
+
+export default App
